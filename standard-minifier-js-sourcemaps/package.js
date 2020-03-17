4dac6e5b25deef89905ddadcf9319271f746a4c8
zodern <zodern@users.noreply.github.com>
2020-03-17 11:37:10 -0500

    Release zodern:standard-minifier-js@4.0.0-beta.4

--- standard-minifier-js-sourcemaps/package.js
+++ standard-minifier-js-sourcemaps/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:standard-minifier-js',
-  version: '4.0.0-beta.3',
+  version: '4.0.0-beta.4',
   summary: 'Javascript minifier that creates production sourcemap',
   documentation: '../readme.md',
   git: 'https://github.com/zodern/minify-js-sourcemaps.git'
@@ -12,7 +12,7 @@ Package.registerBuildPlugin({
     'zodern:minifier-js@3.0.0',
     'babel-compiler@6.18.2 || 7.0.0',
     'ecmascript@0.7.0',
-    'zodern:caching-minifier@0.2.0'
+    'zodern:caching-minifier@0.3.0'
   ],
   sources: [
     'plugin/minify-js.js',
