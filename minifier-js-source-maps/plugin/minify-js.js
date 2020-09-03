bdbc842aa4c0a044cc243db9996bc9d1dcad9b98
zodern <zodern@users.noreply.github.com>
2020-09-02 22:23:05 -0500

    Fix safari issues Applies changes from https://github.com/meteor/meteor/pull/10239

--- minifier-js-source-maps/plugin/minify-js.js
+++ minifier-js-source-maps/plugin/minify-js.js
@@ -19,10 +19,13 @@ meteorJsMinify = function (source, sourcemap, path) {
           "process.env.NODE_ENV": NODE_ENV
         }
       },
+      // Fix issue meteor/meteor#9866, as explained in this comment:
+      // https://github.com/mishoo/UglifyJS2/issues/1753#issuecomment-324814782
+      safari10: true,
       mangle: {
-        // Fix issue meteor/meteor#9866, as explained in this comment:
-        // https://github.com/mishoo/UglifyJS2/issues/1753#issuecomment-324814782
-        safari10: true
+        // Fix safari issue related to catch clause scoping
+        // https://github.com/terser-js/terser/issues/117
+        ie8: true
       },
       sourceMap: {
         content: sourcemap
