e49c6f1b1bd3715e65dd109d1ea38db57a4ab85a
zodern <zodern@users.noreply.github.com>
2023-07-21 12:18:44 -0500

    zodern:standard-minifier-js@5.0.0-beta.2

--- standard-minifier-js-sourcemaps/package.js
+++ standard-minifier-js-sourcemaps/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:standard-minifier-js',
-  version: '5.0.0-beta.1',
+  version: '5.0.0-beta.2',
   summary: 'Fast javascript minifier that creates production sourcemap',
   documentation: '../readme.md',
   git: 'https://github.com/zodern/minify-js-sourcemaps.git'
