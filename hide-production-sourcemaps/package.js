3edc58cef2a32ff3c176878ba88ebfe6c6f02c71
zodern <zodern@users.noreply.github.com>
2024-11-20 12:04:47 -0600

    zodern:hide-production-sourcemaps@1.2.1

--- hide-production-sourcemaps/package.js
+++ hide-production-sourcemaps/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zodern:hide-production-sourcemaps',
-  version: '1.2.0',
+  version: '1.2.1',
   // Brief, one-line summary of the package.
   summary: 'Hide sourcemaps in production',
   // URL to the Git repository containing the source code for this package.
